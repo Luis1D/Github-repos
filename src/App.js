a87ca6b8f623615a5f7e45238388d88b82c4d44d
Luis De Anda <luisdeanda916@outlook.com>
2021-03-28 05:35:37 -0700

    Made a list of urls to the repos on github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [repoList, setRepoList] = useState();
+  const [userPhoto, setUserPhoto] = useState();
+  useEffect(() => {
+    axios.get("https://api.github.com/users/Luis1D/repos")
+      .then(res => {
+        let data = res.data;
+        setRepoList(data);
+        setUserPhoto(data[0].owner.avatar_url)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Github</h1>
+        <div className="user-photo-container">
+        {
+          userPhoto ? <div>
+            <img 
+              src={ userPhoto }
+              alt="user"
+              className="user-photo"
+            />
+          </div> 
+          : null
+        }
+        </div>
       </header>
+      <div className="repo-list-container">
+        {
+          repoList ? repoList.map(repo => {
+            return <a className="repo" href={ repo.clone_url } >
+              <span className="repo-name">{ repo.name }</span>
+            </a>
+          })
+          : null
+        }
+      </div>
     </div>
   );
 }
